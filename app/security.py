21f107d46feaffb90a942a4acf60dc10a7cf67e0
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-02-13 11:52:32 +0300

    Switch to bcrypt for faster password hashing

--- app/security.py
+++ app/security.py
@@ -6,7 +6,7 @@ from passlib.context import CryptContext
 from .config import settings
 
 
-pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
